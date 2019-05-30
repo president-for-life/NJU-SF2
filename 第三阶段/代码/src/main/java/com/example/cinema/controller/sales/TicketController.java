20618e6d2cbd0b5474ef65db21979b6da08fb2d5
NeoWoodley <Neo.Woodley@outlook.com>
2019-05-30 19:51:01 +0800

    TicketController添加退票、出票方法

--- 第三阶段/代码/src/main/java/com/example/cinema/controller/sales/TicketController.java
+++ 第三阶段/代码/src/main/java/com/example/cinema/controller/sales/TicketController.java
@@ -3,6 +3,7 @@ package com.example.cinema.controller.sales;
 import com.example.cinema.bl.sales.TicketService;
 import com.example.cinema.vo.ResponseVO;
 import com.example.cinema.vo.TicketForm;
+import com.example.cinema.vo.TicketRefundStrategyForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,4 +44,38 @@ public class TicketController {
         return ticketService.getBySchedule(scheduleId);
     }
 
+    @PostMapping("/pickUp")
+    public ResponseVO pickUpTicket(@RequestParam int ticketId){
+        return ticketService.pickUpTicket(ticketId);
+    }
+
+    @PostMapping("/refundStrategy/publish")
+    public ResponseVO publishRefundStrategy(@RequestBody TicketRefundStrategyForm ticketRefundStrategyForm) {
+        return ticketService.addRefundStrategy(ticketRefundStrategyForm);
+    }
+
+    @PostMapping("/refundStrategy/update")
+    public ResponseVO updateRefundStrategy(@RequestBody TicketRefundStrategyForm ticketRefundStrategyForm) {
+        return ticketService.updateRefundStrategy(ticketRefundStrategyForm);
+    }
+
+    @PostMapping("/refundStrategy/addMovies")
+    public ResponseVO addRefundMovies(@RequestParam int refundStrategyId, @RequestBody List<Integer> movieIdList) {
+        return ticketService.addRefundMovies(refundStrategyId, movieIdList);
+    }
+
+    @PostMapping("/refundStrategy/removeMovies")
+    public ResponseVO removeRefundMovies(@RequestParam int refundStrategyId, @RequestBody List<Integer> movieIdList) {
+        return ticketService.removeRefundMovies(refundStrategyId, movieIdList);
+    }
+
+    @PostMapping("/refund/choose")
+    public ResponseVO chooseRefundTicket(@RequestParam int ticketId) {
+        return ticketService.addRefundTicket(ticketId);
+    }
+
+    @PostMapping("/refund/confirm")
+    public ResponseVO confirmRefundTicket(@RequestParam int ticketId) {
+        return ticketService.completeRefundTicket(ticketId);
+    }
 }
