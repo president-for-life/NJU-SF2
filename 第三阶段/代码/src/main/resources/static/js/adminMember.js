f103fc2d67103a278c714a0ec239c0926b197f99
171250586 <171250586@smail.nju.edu.cn>
2019-06-01 16:17:41 +0800

    管理员会员卡策略

--- 第三阶段/代码/src/main/resources/static/js/adminMember.js
+++ 第三阶段/代码/src/main/resources/static/js/adminMember.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         strategies.forEach(function (strategy) {
             strategiesDomStr +=
                 "<div class='strategy-container'>" +
-                "    <div class='activity-coupon primary-bg'>" +
+                "    <div class='strategy-item primary-bg' " + "id='strategy-" + strategy.id + "' data-strategy='" + JSON.stringify(strategy) + "'>" +
                 "        <span class='gray-text'>"+strategy.description+"</span>" +
                 "        <span class='title'>价格："+strategy.price+"</span>" +
                 "        <span class='title'>满"+strategy.targetAmount+"减<span class='error-text title'>" + strategy.discountAmount+"</span></span>" +
@@ -31,4 +31,71 @@ $(document).ready(function() {
         });
         $(".content-member").append(strategiesDomStr);
     }
+
+    // 发布会员卡充值优惠策略
+    $("#strategy-form-btn").click(function() {
+        var form = {
+            description: $("#member-description-input").val(),
+            price: $("#member-price-input").val(),
+            targetAmount: $("#member-target-input").val(),
+            discountAmount: $("#member-discount-input").val()
+        };
+
+        postRequest(
+            '/vip/strategy/add',
+            form,
+            function (res) {
+                if(res.success){
+                    getStrategies();
+                    $("#editMemberModal").modal('hide');
+                } else {
+                    alert(res.message);
+                }
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            }
+        );
+    });
+
+    // 修改会员卡充值优惠策略
+    $(document).on('click','.strategy-item',function (e) {
+        var strategy = JSON.parse(e.currentTarget.dataset.strategy);
+
+        $("#member-edit-description-input").val(strategy.description);
+        $("#member-edit-price-input").val(strategy.price);
+        $("#member-edit-target-input").val(strategy.targetAmount);
+        $("#member-edit-discount-input").val(strategy.discountAmount);
+
+        $('#editMemberModal').modal('show');
+
+        $('#editMemberModal')[0].dataset.strategyId = strategy.id;
+    });
+
+    $('#strategy-edit-form-btn').click(function () {
+        var form = {
+            id: Number($('#editMemberModal')[0].dataset.strategyId),
+            price: $("#member-edit-price-input").val(),
+            description: $("#member-edit-description-input").val(),
+            targetAmount: $("#member-edit-target-input").val(),
+            discountAmount : $("#member-edit-discount-input").val()
+        };
+        console.log(form);
+
+        postRequest(
+            '/vip/strategy/update',
+            form,
+            function (res) {
+                if(res.success){
+                    // TODO
+                    getStrategies();
+                } else{
+                    alert(res.message);
+                }
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            }
+        );
+    });
 });
