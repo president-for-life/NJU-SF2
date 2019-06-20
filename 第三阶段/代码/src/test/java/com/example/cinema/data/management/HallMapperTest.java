01d5d4f274d629a274aa70e01979a2e5be5a9ac2
darkXmo <18851989097@163.com>
2019-06-21 00:37:56 +0800

    恢复项目文档

---
+++ 第三阶段/代码/src/test/java/com/example/cinema/data/management/HallMapperTest.java
@@ -0,0 +1,16 @@
+package com.example.cinema.data.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HallMapperTest {
+
+    @Test
+    void insertOneHall() {
+    }
+
+    @Test
+    void updateOneHall() {
+    }
+}
